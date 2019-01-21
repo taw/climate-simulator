dcd740dd2e5a4661a7ccbe9e7561f69f3ffd67c9
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2019-01-21 01:15:31 +0000

    Solar power

--- lib/sun.rb
+++ lib/sun.rb
@@ -1,10 +1,52 @@
+# radians are mostly just annoying
+module Math
+  def self.dcos(deg)
+    cos(deg / 180.0 * Math::PI)
+  end
+
+  def self.dsin(deg)
+    sin(deg / 180.0 * Math::PI)
+  end
+end
+
 class Sun
   def tilt
     23.45
   end
 
   # 0 - winter, 0.5 summer
-  def declination(phase)
-    (-Math.cos(phase * 2 * Math::PI) * tilt).round(2)
+  def declination(season)
+    (-Math.cos(season * 2 * Math::PI) * tilt).round(2)
+  end
+
+  # 0 - midnight, 12 - noon, 24 - midnight
+  def hour_angle(hour)
+    hour %= 24.0
+    ((hour - 12) * 15).round(2)
+  end
+
+  def altitude(lat, season, hour)
+    sin_aa = sin_altitude(lat, season, hour)
+    (Math.asin(sin_aa) * 180.0 / Math::PI).round(2)
+  end
+
+  private def sin_altitude(lat, season, hour)
+    d = declination(season)
+    ha = hour_angle(hour)
+    (
+      Math.dcos(lat) * Math.dcos(d) * Math.dcos(ha) +
+      Math.dsin(lat) * Math.dsin(d)
+    ).round(6)
+  end
+
+  def radiation(lat, season, hour)
+    [0, sin_altitude(lat, season, hour)].max
+  end
+
+  # No easy formula, so just try numerically
+  def daily_radiation(lat, season)
+    ((0..23).map do |h|
+      radiation(lat, season, h)
+    end.sum / 24.0).round(6)
   end
 end
--- spec/sun_spec.rb
+++ spec/sun_spec.rb
@@ -1,14 +1,47 @@
 describe Sun do
   let(:sun) { Sun.new }
+  let(:tilt) { 23.45 }
 
-  it "tilt" do
-    expect(sun.tilt).to eq(23.45)
+  it "#tilt" do
+    expect(sun.tilt).to eq(tilt)
   end
 
-  it "declination" do
+  it "#declination" do
     expect(sun.declination(0.00)).to eq(-23.45)
     expect(sun.declination(0.25)).to eq(  0.00)
     expect(sun.declination(0.50)).to eq(+23.45)
     expect(sun.declination(0.75)).to eq(  0.00)
+
+    expect(sun.declination(8.50)).to eq(+23.45)
+  end
+
+  it "#hour_angle" do
+    expect(sun.hour_angle( 0)).to eq(-180)
+    expect(sun.hour_angle( 1)).to eq(-165)
+    expect(sun.hour_angle(12)).to eq(   0)
+    expect(sun.hour_angle(23)).to eq( 165)
+    expect(sun.hour_angle(24)).to eq(-180)
+    expect(sun.hour_angle(-2)).to eq( 150)
+    expect(sun.hour_angle(50)).to eq(-150)
+  end
+
+  # Tests here are a bit bad...
+
+  it "#altitude" do
+    expect(sun.altitude(-tilt,0.00, 12)).to eq(90)
+    expect(sun.altitude(   0, 0.25, 12)).to eq(90)
+    expect(sun.altitude(tilt, 0.50, 12)).to eq(90)
+    expect(sun.altitude(   0, 0.75, 12)).to eq(90)
+  end
+
+  it "#radiation" do
+    expect(sun.radiation(-tilt,0.00, 12)).to eq(1)
+    expect(sun.radiation(   0, 0.25, 12)).to eq(1)
+    expect(sun.radiation(tilt, 0.50, 12)).to eq(1)
+    expect(sun.radiation(   0, 0.75, 12)).to eq(1)
+  end
+
+  it "#daily_radation" do
+    expect(sun.daily_radiation(0, 0.25)).to eq(0.30123)
   end
 end
